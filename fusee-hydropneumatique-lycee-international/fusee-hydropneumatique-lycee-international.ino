8821eb16fd95997a2afd055265a2ff007db9d00b
schochtlts <lts.schocht@gmail.com>
2023-03-24 22:22:47 +0100

    composant et processus

--- fusee-hydropneumatique-lycee-international/fusee-hydropneumatique-lycee-international.ino
+++ fusee-hydropneumatique-lycee-international/fusee-hydropneumatique-lycee-international.ino
@@ -1,3 +1,16 @@
 void setup(void){}
 
-void loop(void){}
+void loop(void){
+/*
+PROCESSUS DANS loop()
+
+1. Mesurer l'altitude. 
+2. Mesurer l'accélération. 
+
+3. Vérifier si Dh > 0. 
+4. Déclencher le parachute si nécéssaire. 
+
+4. Prendre 2 photos avec les caméras. 
+6. Enregistrer les photos. 
+*/
+}
