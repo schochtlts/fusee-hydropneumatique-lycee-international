907dc8bea55f0d16e09c5bc9d824dbe22ab52d3d
schochtlts <102829193+schochtlts@users.noreply.github.com>
2023-06-02 17:42:20 +0200

    correction de 1 à 0

--- fusee-hydropneumatique-lycee-international/carteSD.h
+++ fusee-hydropneumatique-lycee-international/carteSD.h
@@ -3,54 +3,45 @@
 
 #define CHIP_SELECT 4
 
-int initSD(void)
-{
-	if( !SD.begin(CHIP_SELECT) )
-	{
+int initSD(void){
+	if( !SD.begin(CHIP_SELECT) ){
 		Serial.println("ERREUR: Carte absente ou endommagée !");
-		return 0;
+		return 1
 	}
 
 	Serial.println("NOTE: Carte initialisée !");
-	return 1;
+	return 0
 }
 
-int initWritingToFile(char* path, File* file)
-{
-	if( SD.exists(path) )
-	{
-		if( !SD.remove(path) )
-		{
+int initWritingToFile(char* path, File* file){
+	if( SD.exists(path) ){
+		if( !SD.remove(path) ){
 			Serial.println("ERREUR: Impossible de détruire le fichier " + String(path) + " !");
-			return 0;
+			return 1
 		}
 	}
 
 	*file = SD.open(path, FILE_WRITE);
-	if( !(*file) )
-	{
+	if( !(*file) ){
 		Serial.println("ERREUR: Impossible de créer/lire le fichier " + String(path) + " !");
-		return 0;
+		return 1
 	}
 
 	Serial.println("NOTE: Le fichier " + String(path) + " est en cours de lecture !");
-	return 1;
+	return 0
 }
 
-int writeStringToFile(char* buffer, int size, File* file)
-{
-	if( !file.write(buffer, size) )
-	{
+int writeStringToFile(char* buffer, int size, File* file){
+	if( !file.write(buffer, size) ){
 		Serial.println("ERREUR: Impossible d'écrire dans le fichier !");
-		return 0;
+		return 1
 	}
 
-	if( !file.flush() )
-	{
+	if( !file.flush() ){
 		Serial.println("ERREUR: Impossible d'enregistrer sur la carte SD !");
-		return 0;
+		return 1
 	}
 
 	Serial.println("NOTE: Données enregistrées !");
-	return 1;
+	return 0
 }
